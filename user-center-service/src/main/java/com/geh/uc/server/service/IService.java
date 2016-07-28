dba4ed95711fdc621ab3380bdc989fd30cf0f1df
gehui <gehui@caimao.com>
2016-07-28 10:36:08 +0800

    add new files

--- user-center-service/src/main/java/com/geh/uc/server/service/IService.java
+++ user-center-service/src/main/java/com/geh/uc/server/service/IService.java
@@ -1,6 +1,6 @@
 package com.geh.uc.server.service;
 
-import org.springframework.transaction.annotation.Transactional;
+        import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Ge Hui on 2016/6/18.
